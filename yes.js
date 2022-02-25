38c1832f44888ee4da04641bb78ba6ae92e195e9
WaveTsuname <71974743+Cloudeveloper@users.noreply.github.com>
2022-02-25 00:17:52 +0000

    yes.html

--- yes.js
+++ yes.js
@@ -1,1 +1,6 @@
-?author=Auth-Text&title=Title-Text&description=Desc-Text&color=de85ff&image=hh&redirect=redir
+<title>Website Name</title>
+<meta content="Embed Title" property="og:title" />
+<meta content="Site Description" property="og:description" />
+<meta content="https://embed.com/this-is-the-site-url" property="og:url" />
+<meta content="https://embed.com/embedimage.png" property="og:image" />
+<meta content="#43B581" data-react-helmet="true" name="theme-color" />
